1a4627cff104078657d786f501243a565e37c093
farukvai14320-oss <farukvai14320@gmail.com>
2026-03-30 03:04:29 +0530

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
+from gtts import gTTS
+from moviepy.editor import *
 
+def run():
+    topic = "Amazing AI Facts"
+
+    script = "AI is changing the world faster than you think!"
+
+    tts = gTTS(script)
+    tts.save("voice.mp3")
+
+    clip = ImageClip("https://picsum.photos/720/1280").set_duration(10)
+    audio = AudioFileClip("voice.mp3")
+
+    video = clip.set_audio(audio)
+    video.write_videofile("output.mp4", fps=24)
+
+if __name__ == "__main__":
+    run()
